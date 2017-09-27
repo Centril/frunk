91fb2c65b37f1a0ce6d6a232bed0b055674139c0
Travis CI User <travis@example.org>
2017-09-27 12:20:41 +0000

    Documentation for lloydmeta/frunk@caaa776

---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["frunk_core"] = ["impl&lt;H, Tail&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;H&gt;&gt; for <a class=\"struct\" href=\"frunk_core/hlist/struct.HCons.html\" title=\"struct frunk_core::hlist::HCons\">HCons</a>&lt;H, Tail&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;Tail: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;H&gt;&gt; + <a class=\"trait\" href=\"frunk_core/hlist/trait.HList.html\" title=\"trait frunk_core::hlist::HList\">HList</a>,&nbsp;</span>","impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;T&gt;&gt; for <a class=\"struct\" href=\"frunk_core/hlist/struct.HNil.html\" title=\"struct frunk_core::hlist::HNil\">HNil</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
